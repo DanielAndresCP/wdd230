11c5df83b8cf1b378a56f5968d7124c0982cf693
Daniel Concha <danielconchaparra@gmail.com>
2024-07-12 19:08:36 -0400

    W04 - Team Activity - Weather and Dynamic Links v0.8

---
+++ scripts/weather.js
@@ -0,0 +1,37 @@
+const weatherIcon = document.querySelector("#weather-icon")
+const weatherDesc = document.querySelector("#weather-desc")
+const weatherTemp = document.querySelector("#weather-temp")
+
+const url = "https://api.openweathermap.org/data/2.5/weather?lat=-36.62&lon=-72.13&units=metric&appid=9793a1a87751ab17d8b82cdd4a87b2ad"
+
+
+async function apiFetch(url) {
+    try {
+        const response = await fetch(url)
+        if (response.ok) {
+            const data = await response.json()
+            // console.log(data)
+            displayResults(data)
+        } else {
+            throw Error(await response.text())
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function displayResults(data) {
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    const desc = data.weather[0].main;
+    const temp = Math.round(data.main.temp)
+    // console.log(temp)
+
+    weatherIcon.src = iconsrc
+    weatherIcon.alt = desc
+
+    weatherDesc.textContent = desc
+
+    weatherTemp.textContent = temp
+}
+
+apiFetch(url)
